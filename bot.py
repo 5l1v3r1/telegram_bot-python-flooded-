03f42ea82a1b57d4afbcd67b33713d191008b0c0
IgnatiyOS <ignatii1994-23-10.ru@mail.ru>
2017-09-21 17:28:25 +0700

    Update bot.py
    update /weather + add description for weather

--- bot.py
+++ bot.py
@@ -152,8 +152,9 @@ def weather(bot, update):
     reload(config) 
     user = str(update.message.from_user.id)
     if user in config.admin: #если пользовательский id в списке admin то команда выполняется
-        run_command("wget -O - wttr.in/Barnaul -q")
+        run_command("curl wttr.in/Барнаул?0T&lang=ru")
         bot.sendMessage(chat_id=update.message.chat_id, text=textoutput)
+        # http://wttr.in/:help
 
 #функция команады free
 def free(bot, update):
